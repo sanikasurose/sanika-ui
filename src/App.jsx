57668ec6c62de5a998f0ad1e865d4bb8dea83d17
Sanika Surose <109695485+sanikasurose@users.noreply.github.com>
2026-03-12 14:50:50 -0400

    Implement theme switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+const setTheme = (theme) => {
+  document.documentElement.setAttribute("data-theme", theme);
+};
+
 const App = () => {
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Sanika-UI</h1>
       <p>Component library in progress...</p>
+
+      <button onClick={() => setTheme("")}>Classic Pink Mode</button>
+      <button onClick={() => setTheme("darkPink")}>Dark Pink Mode</button>
+      <button onClick={() => setTheme("devMode")}>Dev Mode</button>
     </div>
   );
 };
